5e5fcd339ae1ad019db64128779a1d3c1968beb0
Evgeny Zuchkov <jenyazuchkov@gmail.com>
2026-03-30 11:42:18 +0300

    lesson 2 task 2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,9 +7,11 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
+let dice1 = Math.floor(Math.random() * 6) + 1 // your code
+let dice2= Math.floor(Math.random() * 6) + 1 // your code
 let isWinningDouble // your code
+
+(dice1 === dice2) > 3 ? isWinningDouble = true : isWinningDouble = false
 
 /*
 console.log('Первый бросок: ' + dice1)
